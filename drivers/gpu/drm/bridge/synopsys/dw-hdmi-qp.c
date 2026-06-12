ad625e65f23eff40030d0252f750b4b2bb460d56
Reina_Sakiria <rs@rs64.net>
2026-06-12 15:39:00 +0900

    test work around : yielding training
    ai generated code

--- drivers/gpu/drm/bridge/synopsys/dw-hdmi-qp.c
+++ drivers/gpu/drm/bridge/synopsys/dw-hdmi-qp.c
@@ -3512,6 +3512,14 @@ static void dw_hdmi_qp_bridge_atomic_enable(struct drm_bridge *bridge,
 		drm_panel_enable(hdmi->panel);
 
 	dw_hdmi_qp_hdcp_enable(hdmi, hdmi->curr_conn->state);
+
+	if (link_cfg && link_cfg->frl_mode) {
+        printk(KERN_INFO "rockchip-hdmi: BSB mode - yielding CPU to let FRL work finish...\n");
+        
+        msleep(50); 
+        
+        printk(KERN_INFO "rockchip-hdmi: BSB mode - resumed.\n");
+    }
 }
 
 static const struct drm_bridge_funcs dw_hdmi_bridge_funcs = {
--- drivers/gpu/drm/rockchip/dw_hdmi-rockchip.c
+++ drivers/gpu/drm/rockchip/dw_hdmi-rockchip.c
@@ -997,8 +997,6 @@ static void hdmi_select_link_config(struct rockchip_hdmi *hdmi,
 
         hdmi->dsc_cap.max_slices = 4;
         hdmi->dsc_cap.all_bpp = false;
-
-		hdmi->dsc_cap.max_lanes = 3;
 	} else if (!max_frl_rate || (tmdsclk < HDMI20_MAX_RATE && mode.clock < HDMI20_MAX_RATE)) {
 		//dev_info(hdmi->dev, "use tmds mode\n");
         printk(KERN_INFO "use tmds mode\n");
